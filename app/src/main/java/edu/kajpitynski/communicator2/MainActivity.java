af6c02b008eba3b0d5854299f5905f692c6f0e71
Kaj Pityński <210035@student.pwr.edu.pl>
2020-05-04 21:11:01 +0200

    Coś

--- app/src/main/java/edu/kajpitynski/communicator2/MainActivity.java
+++ app/src/main/java/edu/kajpitynski/communicator2/MainActivity.java
@@ -5,11 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceInfo;
+import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceRequest;
 import android.os.Bundle;
 import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.net.wifi.p2p.WifiP2pManager.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +24,14 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String SERVICE_NAME = "_wificommunicator";
     private static final String SERVICE_TYPE = "_presence._tcp";
+    private static final int SERVER_PORT = 8888;
 
     private WifiP2pManager manager;
 
     private final IntentFilter intentFilter = new IntentFilter();
     private Channel channel;
     private BroadcastReceiver receiver;
+    private WifiP2pDnsSdServiceRequest serviceRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,29 +39,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Indicates a change in the Wi-Fi P2P status.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WIFI_P2P_STATE_CHANGED_ACTION);
 
         // Indicates a change in the list of available peers.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WIFI_P2P_PEERS_CHANGED_ACTION);
 
         // Indicates the state of Wi-Fi P2P connectivity has changed.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WIFI_P2P_CONNECTION_CHANGED_ACTION);
 
         // Indicates this device's details have changed.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+        intentFilter.addAction(WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 
         manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         channel = manager.initialize(this, getMainLooper(), null);
         receiver = new WiFiDirectBroadcastReceiver();
 
         startRegistration();
+        discoverService();
     }
 
     private void startRegistration() {
+        Map<String, String> record = new HashMap<>();
+        record.put("listenport", String.valueOf(SERVER_PORT));
+        record.put("buddyname", "John Doe" + (int) (Math.random() * 1000));
+        record.put("available", "visible");
+
         WifiP2pDnsSdServiceInfo serviceInfo =
                 WifiP2pDnsSdServiceInfo.newInstance(SERVICE_NAME, SERVICE_TYPE,
-                        null);
-        manager.addLocalService(channel, serviceInfo, new WifiP2pManager.ActionListener() {
+                        record);
+        manager.addLocalService(channel, serviceInfo, new ActionListener() {
             @Override
             public void onSuccess() {
                 Log.d(TAG, "Local service added");
@@ -61,6 +76,51 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFailure(int reason) {
                 Log.d(TAG, "Local service not added");
+            }
+        });
+    }
+
+    private void discoverService() {
+        DnsSdTxtRecordListener txtRecordListener = new DnsSdTxtRecordListener() {
+            @Override
+            public void onDnsSdTxtRecordAvailable(String fullDomainName,
+                                                  Map<String, String> txtRecordMap,
+                                                  WifiP2pDevice srcDevice) {
+                Log.d(TAG, "DnsSdTxtRecord available -" + txtRecordMap.toString());
+            }
+        };
+
+        DnsSdServiceResponseListener serviceListener = new DnsSdServiceResponseListener() {
+            @Override
+            public void onDnsSdServiceAvailable(String instanceName,
+                                                String registrationType, WifiP2pDevice srcDevice) {
+
+            }
+        };
+
+        manager.setDnsSdResponseListeners(channel, serviceListener, txtRecordListener);
+
+        serviceRequest = WifiP2pDnsSdServiceRequest.newInstance();
+        manager.addServiceRequest(channel, serviceRequest, new ActionListener() {
+            @Override
+            public void onSuccess() {
+
+            }
+
+            @Override
+            public void onFailure(int reason) {
+
+            }
+        });
+        manager.discoverServices(channel, new ActionListener() {
+            @Override
+            public void onSuccess() {
+                Log.d(TAG, "Services discovered successfully");
+            }
+
+            @Override
+            public void onFailure(int reason) {
+
             }
         });
     }
