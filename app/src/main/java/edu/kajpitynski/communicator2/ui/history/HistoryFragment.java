0ffa792227eb61e3b1d15e62730f6ceacdae4099
Kaj Pityński <210035@student.pwr.edu.pl>
2020-05-13 19:10:28 +0200

    Porzadki

--- app/src/main/java/edu/kajpitynski/communicator2/ui/history/HistoryFragment.java
+++ app/src/main/java/edu/kajpitynski/communicator2/ui/history/HistoryFragment.java
@@ -1,22 +1,18 @@
 package edu.kajpitynski.communicator2.ui.history;
 
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
-
 import android.content.Context;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.ArrayList;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.List;
 
 import edu.kajpitynski.communicator2.R;
